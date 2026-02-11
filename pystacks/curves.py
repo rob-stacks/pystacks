d3f4aef3adf87e23259b57a4c58342a85b4dcafa
Roberto De Ioris <roberto@stackslabs.com>
2026-02-11 11:52:47 +0100

    fixed coincurve support

--- pystacks/curves.py
+++ pystacks/curves.py
@@ -1,5 +1,12 @@
 try:
-    from .curves_backends.coincurve_backend import generate_private_key
+    from .curves_backends.coincurve_backend import (
+        generate_private_key,
+        recover_public_key_from_signature,
+        get_compressed_public_key,
+        get_uncompressed_public_key,
+        sign,
+        get_public_key,
+    )
 except ImportError:
     from .curves_backends.ecdsa_backend import (
         generate_private_key,
